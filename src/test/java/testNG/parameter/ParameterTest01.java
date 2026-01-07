9d0b2e1a76712bc3ee4055dee34cc312391238a1
TheRuchika <ruchikapromodya@gmail.com>
2026-01-07 16:26:19 +0530

    Adding parameter classes

--- src/test/java/testNG/parameter/ParameterTest01.java
+++ src/test/java/testNG/parameter/ParameterTest01.java
@@ -1,4 +1,15 @@
 package testNG.parameter;
 
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
 public class ParameterTest01 {
+
+    @Test
+    @Parameters({"val1","val2"})
+    public void Sum(@Optional("75") int v1, @Optional("4") int v2){
+        int finalSum = v1 + v2;
+        System.out.println("The final sum of the given value: "+ finalSum);
+    }
 }
